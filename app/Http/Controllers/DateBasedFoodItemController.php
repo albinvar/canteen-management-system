2294ed7bfcf2133896ba8b50212a822c7170f498
Albin Varghese <albin8285@gmail.com>
2022-05-01 01:00:07 +0530

    chore: create model with every other features and create a basic migration for DateBasedFoodItems

---
+++ app/Http/Controllers/DateBasedFoodItemController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DateBasedFoodItem;
+use App\Http\Requests\StoreDateBasedFoodItemRequest;
+use App\Http\Requests\UpdateDateBasedFoodItemRequest;
+
+class DateBasedFoodItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreDateBasedFoodItemRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDateBasedFoodItemRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DateBasedFoodItem  $dateBasedFoodItem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DateBasedFoodItem $dateBasedFoodItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DateBasedFoodItem  $dateBasedFoodItem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DateBasedFoodItem $dateBasedFoodItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateDateBasedFoodItemRequest  $request
+     * @param  \App\Models\DateBasedFoodItem  $dateBasedFoodItem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDateBasedFoodItemRequest $request, DateBasedFoodItem $dateBasedFoodItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DateBasedFoodItem  $dateBasedFoodItem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DateBasedFoodItem $dateBasedFoodItem)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_04_30_192912_create_date_based_food_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('date_based_food_items', static function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('food_item_id')->constrained('food_items');
+            $table->date('date');
+            $table->integer('quantity')->nullable();
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('date_based_food_items');
+    }
+};
