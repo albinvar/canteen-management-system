53e2af385ce3679d70794960120679bac1b66b09
Albin Varghese <albin8285@gmail.com>
2022-05-01 00:04:00 +0530

    chore: create migration to add profile_id to users table

---
+++ database/migrations/2022_04_30_183009_add_profile_id_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('users', static function (Blueprint $table) {
+            $table->foreignId('profile_id')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('users', static function (Blueprint $table) {
+            //remove foreign key
+            $table->dropForeign(['profile_id']);
+            //remove column
+            $table->dropColumn('profile_id');
+        });
+    }
+};
