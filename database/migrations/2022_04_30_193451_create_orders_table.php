4166a23d2e7fc2053ffe21a32d33e85d656fed6c
Albin Varghese <albin8285@gmail.com>
2022-05-01 01:12:08 +0530

    chore: create model with every other features and create a basic migration for Order

---
+++ database/migrations/2022_04_30_193451_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('orders', static function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('food_item_id')->constrained('food_items');
+            $table->integer('quantity');
+            $table->decimal('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
