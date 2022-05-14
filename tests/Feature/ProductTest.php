928be8735b47c58c89e9da019887952889386c22
Albin Varghese <albin8285@gmail.com>
2022-05-14 20:28:20 +0530

    test: create tests for products endpoints

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -1,36 +1,49 @@
 <?php
 
+namespace Tests\Feature;
 
 use App\Models\Product;
-use PHPUnit\Framework\TestCase;
+use App\Models\User;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+use App\Models\Category;
 
-class Test extends TestCase
+class ProductTest extends TestCase
 {
-    // determine if them product class is instantiated
-    public function testProductClassIsInstantiated(): void
-    {
-        $product = new Product();
-        $this->assertInstanceOf(Product::class, $product);
-    }
 
-    // check if the api endpoint shows the list of products
-    public function testProductListIsReturned(): void
-    {
-        $response = $this->get('/api/products');
-        $response->assertStatus(200);
-    }
+    use RefreshDatabase;
 
-    // check if the api endpoint shows the product details
-    public function testProductDetailsIsReturned(): void
+    /**
+     * Check if the products are listed on the index page.
+     *
+     * @return void
+     */
+    public function test_retrieve_all_products(): void
     {
-        $response = $this->get('/api/products/1');
-        $response->assertStatus(200);
-    }
+        $user = User::factory()->create();
 
-    // check if the api endpoint shows the product details
-    public function testProductDetailsIsNotReturned(): void
-    {
-        $response = $this->get('/api/products/100');
-        $response->assertStatus(404);
+        $this->actingAs($user);
+
+        // Create 10 products.
+        $products = Product::factory(10)->create();
+
+        $response = $this->getJson(route('api.admin.products.index'));
+
+        $response->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) => $json->where('ok', true)
+                ->where('products.0.name', $products[0]->name)
+                ->where('products.0.description', $products[0]->description)
+                ->where('products.0.slug', $products[0]->slug)
+                ->where('products.9.name', $products[9]->name)
+                ->where('products.9.description', $products[9]->description)
+                ->where('products.9.slug', $products[9]->slug)
+                ->where('products.9.price', $products[9]->price)
+                ->where('products.9.category_id', $products[9]->category_id)
+                ->missing('products.10')
+                ->etc());
     }
 }
